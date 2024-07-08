27af4b0ee83eec26f90498dc3f2bfd2246adaac3
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-07-08 23:48:17 +0530

    added  new cors origin

--- src/app.js
+++ src/app.js
@@ -8,19 +8,20 @@ import { productRoute } from "./routes/product.route.js";
 import { paymentRoute } from "./routes/payment.route.js";
 import { categoryRoute } from "./routes/category.route.js";
 import { trackingRoute } from "./routes/tracking.route.js";
-import multer from "multer";
+
 
 const app = express();
 
 app.use(cors({
-  origin: 'https://668a8de061200ed8a91150c0--profound-crumble-ce3c7e.netlify.app',
+  origin: 'https://668c2d0bb571f289c626effc--mellow-salmiakki-1c8a58.netlify.app',
   credentials: true
 }));
 
 app.options('*', cors({
-  origin: 'https://668a8de061200ed8a91150c0--profound-crumble-ce3c7e.netlify.app',
+  origin: 'https://668c2d0bb571f289c626effc--mellow-salmiakki-1c8a58.netlify.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true
+  credentials: true,
+  
 }));
 
 
