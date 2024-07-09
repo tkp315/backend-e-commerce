e6c97ceb5ce7fbeb259cf3eebfcba83524d41924
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-07-09 12:56:54 +0530

    yes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,10 +1,6 @@
 import multer from "multer";
-import path from 'path'
-import fs from 'fs'
-const publicDir = path.resolve(__dirname,'../../public')
-if(fs.existsSync(publicDir)){
-  fs.mkdirSync(publicDir,{recursive:true})
-}
+
+const publicDir = "../../public"
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null,publicDir)
