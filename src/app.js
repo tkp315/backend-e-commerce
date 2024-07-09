2f9cfcc1025f6673e8011eea29a0cdeafcc1b2af
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-07-09 13:51:26 +0530

    added new thing

--- src/app.js
+++ src/app.js
@@ -13,13 +13,13 @@ import { trackingRoute } from "./routes/tracking.route.js";
 const app = express();
 
 app.use(cors({
-  origin: 'https://668a8de061200ed8a91150c0--profound-crumble-ce3c7e.netlify.app',
+  origin: 'https://668cf29f9468990a36c23c64--cozy-meerkat-12acd0.netlify.app',
   // origin:"http://localhost:3000",
   credentials: true
 }));
 
 app.options('*', cors({
-  origin: 'https://668a8de061200ed8a91150c0--profound-crumble-ce3c7e.netlify.app',
+  origin: 'https://668cf29f9468990a36c23c64--cozy-meerkat-12acd0.netlify.app',
   // origin:"http://localhost:3000",
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
