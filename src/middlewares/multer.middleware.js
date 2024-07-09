d9dd5c28d8986fe1da66a899ae8fa94ab113e5d5
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-07-09 12:07:32 +0530

    added right directory

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,9 @@
 import multer from "multer";
 
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public");
+    cb(null, "../../public/temp");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
