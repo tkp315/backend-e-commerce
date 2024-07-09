d9f1792f99c16fcbb17706f62702f1850061118b
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-07-09 08:35:16 +0530

    added 1 more origin

--- src/app.js
+++ src/app.js
@@ -13,12 +13,12 @@ import { trackingRoute } from "./routes/tracking.route.js";
 const app = express();
 
 app.use(cors({
-  origin: 'https://668a8de061200ed8a91150c0--profound-crumble-ce3c7e.netlify.app',
+  origin: 'https://668c3714e1b414475d7e8cd1--gorgeous-eclair-e113a2.netlify.app',
   credentials: true
 }));
 
 app.options('*', cors({
-  origin: 'https://668a8de061200ed8a91150c0--profound-crumble-ce3c7e.netlify.app',
+  origin: 'https://668c3714e1b414475d7e8cd1--gorgeous-eclair-e113a2.netlify.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   
