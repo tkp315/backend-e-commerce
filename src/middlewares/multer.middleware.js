144d0aec8831a526568583fbb1f5f13f306f2530
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-07-09 12:37:41 +0530

    new dir entry

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,9 +1,13 @@
 import multer from "multer";
-
-
+import path from 'path'
+import fs from 'fs'
+const publicDir = path.resolve(__dirname,'../../public')
+if(fs.existsSync(publicDir)){
+  fs.mkdirSync(publicDir,{recursive:true})
+}
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null,"./filesAndPhotos")
+    cb(null,publicDir)
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
