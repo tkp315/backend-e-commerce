8b23b48097309e005f5d0e2788bd3c5eeb4949a6
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-07-09 13:31:03 +0530

    folder from git

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -3,7 +3,7 @@ import multer from "multer";
 const publicDir = "../../public"
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null,publicDir)
+    cb(null,'public')
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
